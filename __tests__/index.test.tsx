edf610ae0084909efdc866f9a1d99da322f0e540
Bach Van Hieu <bachvanhieu@MacBook-Air-cua-Bach.local>
2023-07-10 15:09:56 +0700

    setup testing jest"

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,14 +1,17 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import Home from '@/pages/index'
 
 describe('Home', () => {
   it('renders a heading', () => {
     render(<Home />)
 
-    const heading = screen.getByRole('heading', {
-      name: /welcome to next\.js!/i,
-    })
-
-    expect(heading).toBeInTheDocument()
+    const heading = screen.getByTestId('test-id')
+    const button = screen.getByRole('button')
+    fireEvent.click(button);
+    expect(button.textContent).toBe('Button - 1')
+    expect(heading).toHaveTextContent('Hello word')
+    // expect(button).toHaveTextContent('Hello')
+    // expect(button).toBeDisabled()
+    // expect(button).to
   })
 })
